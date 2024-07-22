cd468c5285c8001a7f82a58882d445e5aab9f711
charlie <157563353+ChaDevCN@users.noreply.github.com>
2024-07-22 09:34:00 +0800

    feat: init

--- web-generate.bat
+++ web-generate.bat
@@ -1,6 +1,6 @@
 set path=C:\Users\tennyson\AppData\Roaming\nvm\v18.15.0;%path%
-set DB_NAME=%1%
-set ORIGIN=%2%
-set DOMIN=%3%
+set DB_NAME=icms_en_ee0eefc03da111ef8d0ea3c94e48f577
+set ORIGIN=www.peakrisemetal.com
+set DOMIN=m30_huafeng
 set NODE_ENV=production
 "C:\Users\tennyson\AppData\Roaming\nvm\v18.15.0\npm.cmd" run generate
